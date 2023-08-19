281fb3c41666f408b143a818cf3f93cb3ccf2cf9
PY0517 <134995353+PY0517@users.noreply.github.com>
2023-08-19 08:35:11 +0530

    update

---
+++ feedback/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from feedback.models import feedbackmodel
+
+
+
+
+class feedbackform(forms.ModelForm):
+    class Meta:
+        model=feedbackmodel
+        fields='__all__'
